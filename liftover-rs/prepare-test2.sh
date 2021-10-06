c0862ceff179708cb079cd763f04661ede4b1494
OKAMURA, Yasunobu <okamura@informationsea.info>
2021-10-06 16:09:28 +0900

    add testfiles

---
+++ liftover-rs/prepare-test2.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -xeu -o pipefail
+
+script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+cd "${script_dir}"
+
+DOWNLOAD_DIR=testfiles/ucsc
+
+function download() {
+    FILENAME="$(basename $1)"
+    if [ ! -f "${DOWNLOAD_DIR}/${FILENAME}" ]; then
+        curl -o "${DOWNLOAD_DIR}/${FILENAME}" "$1"
+    fi
+}
+
+mkdir -p "${DOWNLOAD_DIR}"
+
+download https://hgdownload.soe.ucsc.edu/goldenPath/hg38/liftOver/hg38ToHg19.over.chain.gz
+download https://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz
+
+if [ "$(uname)" == 'Darwin' ]; then
+    download http://hgdownload.soe.ucsc.edu/admin/exe/macOSX.x86_64/liftOver
+else
+    download http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/liftOver
+fi
+chmod +x "${DOWNLOAD_DIR}/liftOver"
+
+download https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+download https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz
+
+if [ ! -f "${DOWNLOAD_DIR}/hg19.fa" ]; then
+    gzip -dc "${DOWNLOAD_DIR}/hg19.fa.gz" > "${DOWNLOAD_DIR}/hg19.fa"
+fi
+
+if [ ! -f "${DOWNLOAD_DIR}/hg38.fa" ]; then
+    gzip -dc "${DOWNLOAD_DIR}/hg38.fa.gz" > "${DOWNLOAD_DIR}/hg38.fa"
+fi
+
+test ! -f "${DOWNLOAD_DIR}/hg19.fa.fai" && samtools faidx "${DOWNLOAD_DIR}/hg19.fa"
+test ! -f "${DOWNLOAD_DIR}/hg38.fa.fai" && samtools faidx "${DOWNLOAD_DIR}/hg38.fa"
+test ! -f "${DOWNLOAD_DIR}/hg19-regions.bed" && python3 testfiles/ucsc-scripts/create-test-regions.py testfiles/ucsc/hg19.fa.fai --output testfiles/ucsc/hg19-regions.bed
+test ! -f "${DOWNLOAD_DIR}/hg38-regions.bed" && python3 testfiles/ucsc-scripts/create-test-regions.py testfiles/ucsc/hg38.fa.fai --output testfiles/ucsc/hg38-regions.bed
+
+./testfiles/ucsc/liftOver -minMatch=0.1 -bedPlus=4 -tab "${DOWNLOAD_DIR}/hg19-regions.bed" "${DOWNLOAD_DIR}/hg19ToHg38.over.chain.gz" "${DOWNLOAD_DIR}/hg19-regions-mapped-to-hg38.bed" "${DOWNLOAD_DIR}/hg19-regions-mapped-to-hg38-unmapped.bed"
+./testfiles/ucsc/liftOver -minMatch=0.1 -bedPlus=4 -tab "${DOWNLOAD_DIR}/hg38-regions.bed" "${DOWNLOAD_DIR}/hg38ToHg19.over.chain.gz" "${DOWNLOAD_DIR}/hg38-regions-mapped-to-hg19.bed" "${DOWNLOAD_DIR}/hg38-regions-mapped-to-hg19-unmapped.bed"
