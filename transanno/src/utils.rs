d48f2a44cd6df017559defcaa45060dc4198e0e0
OKAMURA, Yasunobu <okamura@informationsea.info>
2023-11-16 12:30:52 +0900

    Enable rayon reader/writer

--- transanno/src/utils.rs
+++ transanno/src/utils.rs
@@ -1,10 +1,17 @@
 use std::io::{Read, Write};
 use std::path::Path;
 
+use autocompress::io::{RayonReader, RayonWriter};
+
 pub fn open<P: AsRef<Path>>(path: P) -> std::io::Result<impl Read> {
-    autocompress::autodetect_open(path)
+    Ok(RayonReader::new(autocompress::autodetect_open(path)?))
 }
 
 pub fn create<P: AsRef<Path>>(path: P) -> std::io::Result<impl Write> {
-    autocompress::autodetect_create_prefer_bgzip(path, autocompress::CompressionLevel::default())
+    Ok(RayonWriter::new(
+        autocompress::autodetect_create_prefer_bgzip(
+            path,
+            autocompress::CompressionLevel::default(),
+        )?,
+    ))
 }
