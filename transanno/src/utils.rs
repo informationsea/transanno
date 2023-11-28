d55363959c4b26c69598f4bfa2556c14768c2454
OKAMURA, Yasunobu <okamura@informationsea.info>
2023-11-28 13:15:36 +0900

    remove rayon based reader

--- transanno/src/utils.rs
+++ transanno/src/utils.rs
@@ -1,17 +1,13 @@
 use std::io::{Read, Write};
 use std::path::Path;
 
-use autocompress::io::{RayonReader, RayonWriter};
-
 pub fn open<P: AsRef<Path>>(path: P) -> std::io::Result<impl Read> {
-    Ok(RayonReader::new(autocompress::autodetect_open(path)?))
+    Ok(autocompress::autodetect_open(path)?)
 }
 
 pub fn create<P: AsRef<Path>>(path: P) -> std::io::Result<impl Write> {
-    Ok(RayonWriter::new(
-        autocompress::autodetect_create_prefer_bgzip(
-            path,
-            autocompress::CompressionLevel::default(),
-        )?,
-    ))
+    Ok(autocompress::autodetect_create_prefer_bgzip(
+        path,
+        autocompress::CompressionLevel::default(),
+    )?)
 }
